9a0d96efd769356c0a89c69b4cf0b77b57691a02
sawyerpl <sawyerlafrance@aol.com>
2025-02-02 19:36:01 -0700

    display the df

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import plotly.express as px
 import scipy as sp
 # Load the data frame
 vehicles_df = pd.read_csv('vehicles_us.csv')
+vehicles_df['price'] = vehicles_df['price'].astype('float')
+
+
+
 
 # Create a title the app
 st.header('Vehicle Data Analysis')
